c02efd52ec625542e8111bc4f7c04c565d6d2c7b
HyunJung <dnflchfhrdl@naver.com>
2017-05-31 16:39:30 +0900

    translated Ita->Eng

--- vs2010/vs2010/main.c
+++ vs2010/vs2010/main.c
@@ -1168,78 +1168,110 @@ void livelli(int vet[dim][dim],int lv)
 
 }
 
-void disegna_schermo(int vet[dim][dim],int punt,int vit,int lv)                   // Funzione che disegna il campo di gioco da un vettore[n][n]
+void Draws_screen(int vet[dim][dim],int point,int vit,int lv)                  
+	// Funzione che disegna il campo di gioco da un vettore[n][n]
 {
     int i,l;
 
-    printf(" vite: %d       (%d)     punteggio: %d",vit,lv,punt);
+    printf(" life: %d       (%d)     score: %d",vit,lv,punt);
 
     for(i=0;i<dim;i++){
             printf("\n");
         for(l=0;l<dim;l++){
 
-            if(i==0){
-                printf(" -");                             //secondo quali valori trova nel vettore sa se disegnare il giocatore, i nemici o gli altri aspetti grafici
-            }else if(i==dim-1){
+            if(i==0)
+			{
+                printf(" -");                             
+				//According to which values in the vector knows whether to draw the player, the enemies or other graphic aspects
+            }
+			else if(i==dim-1)
+			{
                 printf(" -");
-            }else if(l==0){
-                printf(" | ");
-            }else if(l==dim-1){
+            }
+			else if(l==0)
+			{
                 printf(" | ");
             }
-            else if(vet[i][l]==0){
+			else if(l==dim-1)
+			{
+                printf(" | ");
+            }
+            else if(vet[i][l]==0)
+			{
                 printf("  ");
-            }else if(vet[i][l]==1){
+            }
+			else if(vet[i][l]==1)
+			{
                 printf(" |");
-            }else if(vet[i][l]==2){
+            }
+			else if(vet[i][l]==2)
+			{
                 set_color(11);
                 printf(" A");
                 set_color(15);
-            }else if(vet[i][l]==3){
+            }
+			else if(vet[i][l]==3)
+			{
                 set_color(14);
                 printf(" W");
                 set_color(15);
-            }else if(vet[i][l]==4){
+            }
+			else if(vet[i][l]==4)
+			{
                 set_color(12);
                 printf(" @");
                 set_color(15);
-            }else if(vet[i][l]==5){
+            }
+			else if(vet[i][l]==5)
+			{
                 set_color(6);
                 printf(" U");
                 set_color(15);
-            }else if(vet[i][l]==7){
+            }
+			else if(vet[i][l]==7)
+			{
                 set_color(2);
                 printf(" V");
                 set_color(15);
-            }else if(vet[i][l]==8){
+            }
+			else if(vet[i][l]==8)
+			{
                 set_color(10);
                 printf(" o");
                 set_color(15);
-            }else if(vet[i][l]==10){
+            }
+			else if(vet[i][l]==10)
+			{
                 set_color(3);
                 printf(" Y");
                 set_color(15);
-            }else if(vet[i][l]==12){
+            }
+			else if(vet[i][l]==12)
+			{
                 set_color(4);
                 printf(" W");
                 set_color(15);
-            }else if(vet[i][l]==14){
+            }
+			else if(vet[i][l]==14)
+			{
                 set_color(5);
                 printf(" X");
                 set_color(15);
-            }else if(vet[i][l]==15){
+            }
+			else if(vet[i][l]==15)
+			{
                 set_color(14);
                 printf(" *");
                 set_color(15);
-            }else if(vet[i][l]==16){
+            }
+			else if(vet[i][l]==16)
+			{
                 set_color(12);
                 printf(" @");
                 set_color(15);
             }
         }
     }
-
-
 }
 
 void esplosione(int vet[dim][dim])                       // funzione che cancella le esplosioni dal vattore del campo una volta innescate
